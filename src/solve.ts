b642d29cca1c2dd1e95ab2331695d98b13322312
tairasoul <100938045+tairasoul@users.noreply.github.com>
2024-04-19 14:34:16 +0200

    smal changes

--- src/solve.ts
+++ src/solve.ts
@@ -3,7 +3,7 @@ import { solveCCT } from "./cct/solver";
 import Logs from "/general/logs";
 
 export async function main(ns: NS) {
-    const logger = new Logs(ns, "CCTSolver");
+    const logger = new Logs(ns, `${ns.pid}-CCTSolver`);
     while (true) {
         await ns.sleep(1);
         const result = await solveCCT(ns, logger);
