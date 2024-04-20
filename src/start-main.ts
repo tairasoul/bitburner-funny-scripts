37c81cc38fe56e9f9fd4bee84ec1f8ea3fc6d24f
tairasoul <mjrcoetzee+1@outlook.com>
2024-04-20 15:01:11 +0200

    more foolery

--- src/start-main.ts
+++ src/start-main.ts
@@ -1,7 +1,6 @@
 import ns from "@ns";
 
 export async function main(ns: ns.NS) {
-    ns.run("solve.js");
     ns.run("services/port-registry.js");
     ns.run("services/ramnet-service.js");
     ns.run("custom-stats.js");
