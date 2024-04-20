4c525b2fccdc5f75dfc4a626e3ade5f8a0de2c09
tairasoul <mjrcoetzee+1@outlook.com>
2024-04-20 09:55:10 +0200

    remove more debugging

--- src/infect/controller.ts
+++ src/infect/controller.ts
@@ -11,6 +11,8 @@ export async function main(ns: ns.NS) {
     ns.disableLog("exec");
     ns.disableLog("getScriptRam");
     ns.disableLog("scp");
+    ns.disableLog("getServerMaxRam");
+    ns.disableLog("getServerUsedRam");
     const targetServer = ns.args[0] as string;
     const commsPort = ns.args[1] as number;
     const ramnet = new RamnetComms(ns);
@@ -26,12 +28,11 @@ export async function main(ns: ns.NS) {
     ns.print(`ram on ramnet dedicated per controller: ${ramnetDedicated}`);
     const portComms = new Communicator(ns);
     const ports = (await portComms.assignFirstAvailable(1));
-    const returnPorts = await portComms.assignFirstAvailable(20);
+    const returnPorts = await portComms.assignFirstAvailable(5);
     const startSignal = ports.assignedPorts[0];
     ns.atExit(() => {
         portComms.unassignPorts(ports.assignedPorts)
         portComms.unassignPorts(returnPorts.assignedPorts);
-        ns.rm(`/lock/controllers/${targetServer}.txt`, "home");
     });
     const returnPort = new Multiport(ns, {ports: returnPorts.assignedPorts})
     await ns.sleep(50);
@@ -61,30 +62,35 @@ export async function main(ns: ns.NS) {
             while (true) {
                 await ns.sleep(1);
                 const jobsAssigned: Job[] = [];
+                let jobsDone = 0;
                 while (true) {
                     const newJob = await ramnet.assignJob(jobs.grow);
                     const server = newJob.jobAssigned.server;
-                    const serverRam = ns.getServerMaxRam(server) - ns.getServerUsedRam(server);
-                    let threads = Math.floor(serverRam / ns.getScriptRam(growS, "home"));
-                    let ramUsage = ns.getScriptRam(growS, "home") * threads;
-                    if (ramUsed + ramUsage >= ramnetDedicated) {
-                        while (ramUsed + ramUsage >= ramnetDedicated) {
-                            if (threads == 0) break;
-                            threads -= 1;
-                            ramUsage = ns.getScriptRam(growS, "home") * threads;
+                    if (server != '') {
+                        const serverRam = ns.getServerMaxRam(server) - ns.getServerUsedRam(server);
+                        let threads = Math.floor(serverRam / ns.getScriptRam(growS, "home"));
+                        let ramUsage = ns.getScriptRam(growS, "home") * threads;
+                        if (ramUsed + ramUsage >= ramnetDedicated) {
+                            while (ramUsed + ramUsage >= ramnetDedicated) {
+                                if (threads == 0) break;
+                                threads -= 1;
+                                ramUsage = ns.getScriptRam(growS, "home") * threads;
+                            }
+                        }
+                        ramUsed += ramUsage;
+                        await ramnet.finishJob(newJob.jobAssigned);
+                        if (ramUsed >= ramnetDedicated || threads == 0) {
+                            ramUsed -= ramUsage;
+                            break;
+                        }
+                        const realJob = await ramnet.assignJob({ram: ramUsage, server: ""});
+                        if (realJob.jobAssigned.server != '') {
+                            jobsDone += 1;
+                            jobsAssigned.push(realJob.jobAssigned)
+                            copyScripts(ns, realJob.jobAssigned.server);
+                            grow(ns, threads, realJob.jobAssigned.server, returnPorts.assignedPorts, {server: targetServer, startPort: startSignal});
                         }
                     }
-                    ramUsed += ramUsage;
-                    await ramnet.finishJob(newJob.jobAssigned);
-                    ns.print(`ram used: ${ramUsed}/${ramnetDedicated}`);
-                    if (ramUsed >= ramnetDedicated || threads == 0) {
-                        ramUsed -= ramUsage;
-                        break;
-                    }
-                    const realJob = await ramnet.assignJob({ram: ramUsage, server: ""});
-                    jobsAssigned.push(realJob.jobAssigned)
-                    copyScripts(ns, realJob.jobAssigned.server);
-                    grow(ns, threads, realJob.jobAssigned.server, returnPorts.assignedPorts, {server: targetServer, startPort: startSignal});
                 }
                 let jobsFinished = 0;
                 ns.print(`making sure all scripts can get start signal..`);
@@ -96,12 +102,14 @@ export async function main(ns: ns.NS) {
                     if (!returnPort.empty()) {
                         returnPort.read();
                         jobsFinished++;
+                        ns.print(`job ${jobsFinished}/${jobsDone} finished`)
                     }
-                    if (jobsFinished == jobsAssigned.length - 1) {
+                    if (jobsFinished == jobsDone) {
                         ns.print(`telling ramnet the jobs are finished (${jobsFinished} jobs)`)
                         for (const job of jobsAssigned) {
                             await ramnet.finishJob(job);
                         }
+                        ramUsed = 0;
                         break;
                     }
                 }
@@ -113,30 +121,35 @@ export async function main(ns: ns.NS) {
             while (true) {
                 await ns.sleep(1);
                 const jobsAssigned: Job[] = [];
+                let jobsDone = 0;
                 while (true) {
                     const newJob = await ramnet.assignJob(jobs.grow);
                     const server = newJob.jobAssigned.server;
-                    const serverRam = ns.getServerMaxRam(server) - ns.getServerUsedRam(server);
-                    let threads = Math.floor(serverRam / ns.getScriptRam(weakenS, "home"));
-                    let ramUsage = ns.getScriptRam(weakenS, "home") * threads;
-                    if (ramUsed + ramUsage >= ramnetDedicated) {
-                        while (ramUsed + ramUsage >= ramnetDedicated) {
-                            if (threads == 0) break;
-                            threads -= 1;
-                            ramUsage = ns.getScriptRam(growS, "home") * threads;
+                    if (server != '') {
+                        const serverRam = ns.getServerMaxRam(server) - ns.getServerUsedRam(server);
+                        let threads = Math.floor(serverRam / ns.getScriptRam(weakenS, "home"));
+                        let ramUsage = ns.getScriptRam(weakenS, "home") * threads;
+                        if (ramUsed + ramUsage >= ramnetDedicated) {
+                            while (ramUsed + ramUsage >= ramnetDedicated) {
+                                if (threads == 0) break;
+                                threads -= 1;
+                                ramUsage = ns.getScriptRam(weakenS, "home") * threads;
+                            }
+                        }
+                        ramUsed += ramUsage;
+                        await ramnet.finishJob(newJob.jobAssigned);
+                        if (ramUsed >= ramnetDedicated || threads == 0) {
+                            ramUsed -= ramUsage;
+                            break;
+                        }
+                        const realJob = await ramnet.assignJob({ram: ramUsage, server: ""});
+                        if (realJob.jobAssigned.server != '') {
+                            jobsDone += 1;
+                            jobsAssigned.push(realJob.jobAssigned)
+                            copyScripts(ns, realJob.jobAssigned.server);
+                            weaken(ns, threads, realJob.jobAssigned.server, returnPorts.assignedPorts, {server: targetServer, startPort: startSignal});
                         }
                     }
-                    ramUsed += ramUsage;
-                    await ramnet.finishJob(newJob.jobAssigned);
-                    ns.print(`ram used: ${ramUsed}/${ramnetDedicated}`);
-                    if (ramUsed >= ramnetDedicated || threads == 0) {
-                        ramUsed -= ramUsage;
-                        break;
-                    }
-                    const realJob = await ramnet.assignJob({ram: ramUsage, server: ""});
-                    jobsAssigned.push(realJob.jobAssigned)
-                    copyScripts(ns, realJob.jobAssigned.server);
-                    weaken(ns, threads, realJob.jobAssigned.server, returnPorts.assignedPorts, {server: targetServer, startPort: startSignal});
                 }
                 let jobsFinished = 0;
                 ns.print(`making sure all scripts can get start signal..`);
@@ -148,12 +161,14 @@ export async function main(ns: ns.NS) {
                     if (!returnPort.empty()) {
                         returnPort.read();
                         jobsFinished++;
+                        ns.print(`job ${jobsFinished}/${jobsDone} finished`)
                     }
-                    if (jobsFinished == jobsAssigned.length - 1) {
+                    if (jobsFinished == jobsDone) {
                         ns.print(`telling ramnet the jobs are finished (${jobsFinished} jobs)`)
                         for (const job of jobsAssigned) {
                             await ramnet.finishJob(job);
                         }
+                        ramUsed = 0;
                         break;
                     }
                 }
@@ -164,30 +179,35 @@ export async function main(ns: ns.NS) {
         while (true) {
             await ns.sleep(1);
             const jobsAssigned: Job[] = [];
+            let jobsDone = 0;
             while (true) {
                 const newJob = await ramnet.assignJob(jobs.grow);
                 const server = newJob.jobAssigned.server;
-                const serverRam = ns.getServerMaxRam(server) - ns.getServerUsedRam(server);
-                let threads = Math.floor(serverRam / ns.getScriptRam(hackS, "home"));
-                let ramUsage = ns.getScriptRam(hackS, "home") * threads;
-                if (ramUsed + ramUsage >= ramnetDedicated) {
-                    while (ramUsed + ramUsage >= ramnetDedicated) {
-                        if (threads == 0) break;
-                        threads -= 1;
-                        ramUsage = ns.getScriptRam(growS, "home") * threads;
+                if (server != '') {
+                    const serverRam = ns.getServerMaxRam(server) - ns.getServerUsedRam(server);
+                    let threads = Math.floor(serverRam / ns.getScriptRam(hackS, "home"));
+                    let ramUsage = ns.getScriptRam(hackS, "home") * threads;
+                    if (ramUsed + ramUsage >= ramnetDedicated) {
+                        while (ramUsed + ramUsage >= ramnetDedicated) {
+                            if (threads == 0) break;
+                            threads -= 1;
+                            ramUsage = ns.getScriptRam(hackS, "home") * threads;
+                        }
+                    }
+                    ramUsed += ramUsage;
+                    await ramnet.finishJob(newJob.jobAssigned);
+                    if (ramUsed >= ramnetDedicated || threads == 0) {
+                        ramUsed -= ramUsage;
+                        break;
+                    }
+                    const realJob = await ramnet.assignJob({ram: ramUsage, server: ""});
+                    if (realJob.jobAssigned.server != '') {
+                        jobsDone += 1;
+                        jobsAssigned.push(realJob.jobAssigned)
+                        copyScripts(ns, realJob.jobAssigned.server);
+                        hack(ns, threads, realJob.jobAssigned.server, returnPorts.assignedPorts, {server: targetServer, startPort: startSignal});
                     }
                 }
-                ramUsed += ramUsage;
-                await ramnet.finishJob(newJob.jobAssigned);
-                ns.print(`ram used: ${ramUsed}/${ramnetDedicated}`);
-                if (ramUsed >= ramnetDedicated || threads == 0) {
-                    ramUsed -= ramUsage;
-                    break;
-                }
-                const realJob = await ramnet.assignJob({ram: ramUsage, server: ""});
-                jobsAssigned.push(realJob.jobAssigned)
-                copyScripts(ns, realJob.jobAssigned.server);
-                hack(ns, threads, realJob.jobAssigned.server, returnPorts.assignedPorts, {server: targetServer, startPort: startSignal});
             }
             let jobsFinished = 0;
             ns.print(`making sure all scripts can get start signal..`);
@@ -199,12 +219,14 @@ export async function main(ns: ns.NS) {
                 if (!returnPort.empty()) {
                     returnPort.read();
                     jobsFinished++;
+                    ns.print(`job ${jobsFinished}/${jobsDone} finished`)
                 }
-                if (jobsFinished == jobsAssigned.length - 1) {
+                if (jobsFinished == jobsDone) {
                     ns.print(`telling ramnet the jobs are finished (${jobsFinished} jobs)`)
                     for (const job of jobsAssigned) {
                         await ramnet.finishJob(job);
                     }
+                    ramUsed = 0;
                     break;
                 }
             }
@@ -233,5 +255,8 @@ function deployScript(ns: ns.NS, threads: number, script: string, server: string
 }
 
 function copyScripts(ns: ns.NS, server: string) {
-    ns.scp(["/infect/worms/grow.js", "/infect/worms/hack.js", "/infect/worms/weaken.js", "/general/multiport.js"], server, "home")
+    try {
+        ns.scp(["/infect/worms/grow.js", "/infect/worms/hack.js", "/infect/worms/weaken.js", "/general/multiport.js"], server, "home")
+    }
+    catch {}
 }
