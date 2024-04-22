4ad64eefa26c277773b0a088b0c5cfd1637daeea
tairasoul <100938045+tairasoul@users.noreply.github.com>
2024-04-22 11:18:35 +0200

    more robust auto-roulette

--- src/rouletteburn-automated/begin.ts
+++ src/rouletteburn-automated/begin.ts
@@ -2,13 +2,20 @@ import { NS } from "@ns";
 import { download } from "/rouletteburn-automated/download";
 import { aevum } from "/rouletteburn-automated/go-to-aevum";
 import { cheat } from "/rouletteburn-automated/start-cheating";
-import { exploit } from "/rouletteburn-automated/game/get-game";
 
 export async function main(ns: NS) {
     if (!ns.fileExists("rouletteburn.js"))
         await download(ns);
-    const game = await exploit();
-    if (game.city != "Aevum")
+    const doc = eval("document") as Document;
+    const divsWithRoleButton = doc.querySelectorAll('div[role="button"]');
+    divsWithRoleButton.forEach(div => {
+        if (Array.from(div.querySelectorAll('*')).some(element => element.textContent?.includes('City'))) {
+            (div as HTMLElement).click();
+        }
+    });
+    const gameWindow = doc.querySelector("#root > div.MuiBox-root > div.MuiBox-root") as HTMLDivElement;
+    const city = gameWindow.querySelector(':nth-child(1)')?.textContent as string;
+    if (city.trim() != "Aevum")
         await aevum(ns);
     await cheat(ns)
 }
--- src/rouletteburn-automated/go-to-aevum.ts
+++ src/rouletteburn-automated/go-to-aevum.ts
@@ -1,29 +1,48 @@
 import ns from "@ns";
+import { gainAccess } from "/infect/utils";
+import { exploit } from "/game/get-game";
 
 export async function aevum(ns: ns.NS) {
     const doc = eval("document") as Document;
+    const game = await exploit();
+    if (game.money < 200000) {
+        const hacking = game.skills.hacking;
+        const priority = ["omega-net", "phantasy", "silver-helix", "iron-gym", "zer0", "max-hardware", "CSEC", "harakiri-sushi", "joesguns", "sigma-cosmetics", "foodnstuff", "n00dles"];
+        for (const server of priority) {
+            const skillRequired = ns.getServer(server).requiredHackingSkill as number;
+            if (hacking >= skillRequired) {
+                const accessGained = await gainAccess(ns, server);
+                if (accessGained.nuke) {
+                    ns.toast(`money too low, hacking ${server} until we have enough money!`, "error", 3000);
+                    const ram = ns.getServerMaxRam(ns.getHostname()) - ns.getServerUsedRam(ns.getHostname());
+                    const hackUsage = ns.getScriptRam("/rouletteburn-automated/goto/gain-money.js");
+                    const threads = Math.floor(ram / hackUsage);
+                    const pid = ns.run("/rouletteburn-automated/goto/gain-money.js", threads, server);;
+                    while (ns.isRunning(pid))
+                        await ns.sleep(1);
+                    break;
+                }
+            }
+        }
+    }
     const buttons = doc.querySelectorAll("div.MuiButtonBase-root");
-
     buttons.forEach(div => {
         if (Array.from(div.querySelectorAll('*')).some(element => element.textContent?.includes('Travel'))) {
             (div as HTMLElement).click();
         }
     });
     const elems = doc.querySelectorAll('span');
-    let element: HTMLElement | undefined;
     elems.forEach((elem) => {
         if (elem.textContent?.trim() == "A") {
-            element = elem as HTMLElement;
+            (elem as HTMLElement).click()
             return;
         }
     })
-    if (ns.getServerMoneyAvailable("home") < 200000) {
-        while (ns.getServerMoneyAvailable("home") < 200000) {
-            await ns.hack("n00dles");
-            await ns.weaken("n00dles");
-            await ns.grow("n00dles");
-            await ns.weaken("n00dles");
+
+    const divs = doc.querySelectorAll("button.MuiButtonBase-root");
+    divs.forEach(div => {
+        if (Array.from(div.querySelectorAll('*')).some(element => element.textContent?.includes('Travel'))) {
+            (div as HTMLElement).click();
         }
-    }
-    if (element) element.click();
+    })
 }
