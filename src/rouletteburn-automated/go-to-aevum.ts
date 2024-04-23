dcc0f831c2e04fae352013f1eaa9a7ac73335e4c
tairasoul <mjrcoetzee+1@outlook.com>
2024-04-23 11:25:56 +0200

    hopefully more robust so itll get you the 200k needed faster

--- src/rouletteburn-automated/go-to-aevum.ts
+++ src/rouletteburn-automated/go-to-aevum.ts
@@ -1,9 +1,11 @@
 import ns from "@ns";
 import { gainAccess } from "/infect/utils";
 import { exploit } from "/game/get-game";
+import PortCommunicator from "/service-communicators/port-registry";
 
 export async function aevum(ns: ns.NS) {
     const doc = eval("document") as Document;
+    const comms = new PortCommunicator(ns);
     const game = await exploit();
     if (game.money < 200000) {
         const hacking = game.skills.hacking;
@@ -13,13 +15,16 @@ export async function aevum(ns: ns.NS) {
             if (hacking >= skillRequired) {
                 const accessGained = await gainAccess(ns, server);
                 if (accessGained.nuke) {
+                    const port = await comms.assignFirstAvailable(1);
                     ns.toast(`money too low, hacking ${server} until we have enough money!`, "error", 3000);
                     const ram = ns.getServerMaxRam(ns.getHostname()) - ns.getServerUsedRam(ns.getHostname());
                     const hackUsage = ns.getScriptRam("/rouletteburn-automated/goto/gain-money.js");
                     const threads = Math.floor(ram / hackUsage);
-                    const pid = ns.run("/rouletteburn-automated/goto/gain-money.js", threads, server);;
-                    while (ns.isRunning(pid))
-                        await ns.sleep(1);
+                    ns.run("/rouletteburn-automated/goto/gain-money.js", threads, server, port.assignedPorts[0]);
+                    const handle = ns.getPortHandle(port.assignedPorts[0]);
+                    await handle.nextWrite();
+                    handle.clear();
+                    await comms.unassignPorts(port.assignedPorts);
                     break;
                 }
             }
--- src/rouletteburn-automated/goto/gain-money.ts
+++ src/rouletteburn-automated/goto/gain-money.ts
@@ -2,10 +2,13 @@ import { NS } from "@ns";
 
 export async function main(ns: NS) {
     const server = ns.args[0] as string;
+    const port = ns.args[1] as number;
+    const handle = ns.getPortHandle(port);
     while (ns.getServerMoneyAvailable("home") < 200000) {
         await ns.hack(server);
         await ns.weaken(server);
         await ns.grow(server);
         await ns.weaken(server);
     }
+    handle.write('done');
 }
